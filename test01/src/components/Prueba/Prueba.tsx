de4ca4a935787525ed91f21dcead3c23d0063db7
JsanchezCorp <juansanchezcorporativo@gmail.com>
2025-03-28 00:31:50 -0500

    commit first test ionic

---
+++ test01/src/components/Prueba/Prueba.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Prueba: React.FC = () => {
+  return (
+    <div>
+      <p>Este es un nuevo componente de prueba</p>
+    </div>
+  );
+};
+
+export default Prueba;
